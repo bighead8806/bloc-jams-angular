0a997271f46eb5735218b848d12886f8b09b1cb8
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2017-06-04 22:31:04 -0600

    added some new desc. and dynamic song/album info

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -8,7 +8,12 @@
         */
         SongPlayer.currentSong = null;
          
-        var getSongIndex = function(song) {
+       /**
+       *@function getSongIndex
+       *@desc returns index of current album's song list 
+       */
+         
+         var getSongIndex = function(song) {
              return currentAlbum.songs.indexOf(song);
          };
          
@@ -26,8 +31,7 @@
          
          var setSong = function(song) {
             if (currentBuzzObject) {
-                currentBuzzObject.stop();
-                SongPlayer.currentSong.playing = null;
+                stopSong(song);
             }
             
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -72,20 +76,34 @@
                 formats: ['mp3'],
                 preload: true
             });
+          
+         /**
+          *@function SongPlayer.previous
+          *@desc Play previous song. If currentSongIndex is less than zero, just stop the song. If it is greater than zero  play the previous song. 
+          */
          
           SongPlayer.previous = function() {
             var currentSongIndex = getSongIndex(SongPlayer.currentSong);
             currentSongIndex--;
             
             if (currentSongIndex < 0) {
-                currentBuzzObject.stop();
-                SongPlayer.currentSong.playing = null;
+                stopSong(song);
             } else {
                 var song = currentAlbum.songs[currentSongIndex];
                 setSong(song);
                 playSong(song);
             }
         };
+         
+         /**
+         *@function SongPlayer.next
+         *@desc Play the next song in the index
+         */
+         
+            SongPlayer.next = function(){
+                var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+                currentSongIndex++;
+            }
          
           return SongPlayer;
          
@@ -94,13 +112,25 @@
     /**
     *@function playSong
     *@desc Starts playing a song
+    *@param {Object} song
     */
   
-  var playSong = function(song) {
+    var playSong = function(song) {
             currentBuzzObject.play();
             song.playing = true;
         };
- 
+    
+    /**
+    *@function stopSong
+    *@dec Stops playing a song
+    *@param {Object} song
+    */
+    
+    var stopSong = function(song){
+        currentBuzzObject.stop();
+        song.playing = null;
+    };
+    
      angular
          .module('blocJams')
          .factory('SongPlayer', ['Fixtures', SongPlayer]);
