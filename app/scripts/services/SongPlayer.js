73d9693f05d5a90d3aa7e2e1474f433bd5a0fe1b
Adam Mohrbacher <adam.mohrbacher@gmail.com>
2017-06-07 19:46:24 -0600

    got player bar functionality to work

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -8,15 +8,6 @@
         */
         SongPlayer.currentSong = null;
          
-       /**
-       *@function getSongIndex
-       *@desc returns index of current album's song list 
-       */
-         
-         var getSongIndex = function(song) {
-             return currentAlbum.songs.indexOf(song);
-         };
-         
         /**
         * @desc Buzz object audio file
         * @type {Object}
@@ -42,18 +33,38 @@
             SongPlayer.currentSong = song;
         };
          
-          /**
+            /**
     *@function playSong
     *@desc Starts playing a song
+    *@param {Object} song
     */
   
-           var playSong = function(song) {
-                currentBuzzObject.play();
-                song.playing = true;
-            };
-        
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+    
+    /**
+    *@function stopSong
+    *@dec Stops playing a song
+    *@param {Object} song
+    */
+    
+        var stopSong = function(song){
+            currentBuzzObject.stop();
+            song.playing = null;
+        };
+         
+       /**
+       *@function getSongIndex
+       *@desc returns index of current album's song list. Will be used for next and previous buttons.
+       */
+         var getSongIndex = function(song) {
+             return currentAlbum.songs.indexOf(song);
+         };  
+         
         /**
-        *@function SongPLayer.play
+        *@function SongPlayer.play
         *@desc checks if current song is the same as the song passed to the function and if it is not it starts to playing the new song. If it is equal then it pauses currently playing song and begins playing passed song. 
         *@param {Object}song
         */ 
@@ -64,7 +75,7 @@
                 playSong(song);
 
            }else if(SongPlayer.currentSong === song){
-                if(currentBuzzObject.isPaused()){
+                 if(currentBuzzObject.isPaused()){
                     playSong(song);
                 }
             }
@@ -108,32 +119,19 @@
             SongPlayer.next = function(){
                 var currentSongIndex = getSongIndex(SongPlayer.currentSong);
                 currentSongIndex++;
+                
+                if (currentSongIndex > currentAlbum.length) {
+                    stopSong(SongPlayer.currentSong);
+                } 
+                else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+                }
             }
-         
-          return SongPlayer;
+                return SongPlayer;
          
         }
-    /**
-    *@function playSong
-    *@desc Starts playing a song
-    *@param {Object} song
-    */
-  
-    var playSong = function(song) {
-            currentBuzzObject.play();
-            song.playing = true;
-        };
-    
-    /**
-    *@function stopSong
-    *@dec Stops playing a song
-    *@param {Object} song
-    */
-    
-    var stopSong = function(song){
-        currentBuzzObject.stop();
-        song.playing = null;
-    };
     
      angular
          .module('blocJams')
